828176a7f728041cb2fe0c2199f5e454cc7af126
shellingfordly <shellingfordly@qq.com>
2023-06-14 17:35:40 +0800

    feat: todo list

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref, watchEffect } from 'vue';
+
+const list = ref(["睡觉", "吃饭"])
+const something = ref("")
+
+const add = () => (something.value && list.value.push(something.value));
+</script>
+
+
+<template>
+	<input v-model="something" />
+	<button @click="add()">ADD</button>
+
+	<ul>
+		<li v-for="item in list"> {{ item }}</li>
+	</ul>
+</template>
